9138a8aa17a91b9ea36f0b35fb6677fa76cff71f
FlyingSky-CN <me@fsky7.com>
2020-01-17 13:34:32 +0800

    ddfg

--- Autohash.php
+++ Autohash.php
@@ -1,8 +1,8 @@
 <?php
 
 //$files = array('404.php','archive.php','comments.php','footer.php','functions.php','header.php','index.php','LICENSE','page.php','page-archives.php','page-links.php','page-whisper.php','post.php','README.md','screenshot.png','sidebar.php','style.css');
-if (isset($argv[0])) {
-    if ($argv[0] == 'hash') {
+if (isset($argv[1])) {
+    if ($argv[1] == 'hash') {
 $files=array('README.md','Autohash.php');
 foreach ($files as $file) {
     $hash.=hash('sha256', file_get_contents($file)).' '.$file."\n";
