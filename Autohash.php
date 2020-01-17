3a6f4aebc6e0acfa748bce6da08a0428dd7a9cea
FlyingSky-CN <me@fsky7.com>
2020-01-17 13:24:28 +0800

    dsfsdgdghhef

---
+++ Autohash.php
@@ -0,0 +1,4 @@
+<?php
+
+$files = array('404.php','archive.php','comments.php','footer.php','functions.php','header.php','index.php','LICENSE','page.php','page-archives.php','page-links.php','page-whisper.php','post.php','README.md','screenshot.png','sidebar.php','style.css');
+$files=array('README.md','Autohash.php');foreach($files as $file){$hash.=hash('sha256', file_get_contents($file)).' '.$file."\n";}file_put_contents("hash.txt",$hash);
