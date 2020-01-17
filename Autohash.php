2ab4f2d3b358e8db4e2c734c819e4fe30026321e
FlyingSky-CN <me@fsky7.com>
2020-01-17 13:27:33 +0800

    sdghfdshfshjgshjg

--- Autohash.php
+++ Autohash.php
@@ -1,4 +1,8 @@
 <?php
 
-$files = array('404.php','archive.php','comments.php','footer.php','functions.php','header.php','index.php','LICENSE','page.php','page-archives.php','page-links.php','page-whisper.php','post.php','README.md','screenshot.png','sidebar.php','style.css');
-$files=array('README.md','Autohash.php');foreach($files as $file){$hash.=hash('sha256', file_get_contents($file)).' '.$file."\n";}file_put_contents("hash.txt",$hash);
+//$files = array('404.php','archive.php','comments.php','footer.php','functions.php','header.php','index.php','LICENSE','page.php','page-archives.php','page-links.php','page-whisper.php','post.php','README.md','screenshot.png','sidebar.php','style.css');
+$files=array('README.md','Autohash.php');
+foreach ($files as $file) {
+    $hash.=hash('sha256', file_get_contents($file)).' '.$file."\n";
+}
+file_put_contents("hash.txt",$hash);
